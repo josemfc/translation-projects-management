874569365231e047deb79c6c43debc68ba991296
jose <josemaria_f_c@hotmail.com>
2015-05-31 18:51:02 +0200

    Solving staticfiles issue 6

--- traducciones/settings.py
+++ traducciones/settings.py
@@ -145,5 +145,5 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    'compressor.finders.CompressorFinder',
+#    'compressor.finders.CompressorFinder',
 )
