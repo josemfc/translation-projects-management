3ad782babd79d27e34f760622e0a30e69cfd9651
jose <josemaria_f_c@hotmail.com>
2015-06-05 19:55:40 +0200

    Fixing bug

--- proyectos/models.py
+++ proyectos/models.py
@@ -27,8 +27,10 @@ class Tarea(models.Model):
 	comentario = models.CharField(max_length=300, null=True, blank=True)
 	terminada = models.BooleanField(default=False)
 	num_horas = models.IntegerField('Num. de horas estimado')
-	original = models.FileField('Texto original', upload_to=upload_path_handler, null=True, blank=True)
-	traducido = models.FileField('Texto traducido', upload_to=upload_path_handler, null=True, blank=True)
+	#original = models.FileField('Texto original', upload_to=upload_path_handler, null=True, blank=True)
+	#traducido = models.FileField('Texto traducido', upload_to=upload_path_handler, null=True, blank=True)
+	original = models.FileField('Texto original', null=True, blank=True)
+	traducido = models.FileField('Texto traducido', null=True, blank=True)
 	TIPO_TAREA = (
 		('T', 'Traducción'),
 		('R', 'Revisión'),
